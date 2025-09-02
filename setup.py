6844707e75357a853ac6fdf032b1440ffaeeef35
Akshat482002 <akshatpratapsingh482002@gmail.com>
2025-09-02 21:18:55 +0530

    new

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Akshat Pratap Singh',
+    author_email= 'akshatpratapsingh482002@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
